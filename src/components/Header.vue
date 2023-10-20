82ac82a68220210013e20bfd42d864965ea347e5
Andreea Ciobota <135983461+Andreeaciobota@users.noreply.github.com>
2023-10-20 14:46:33 +0100

    [Add]:menu

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,12 +1,55 @@
 <template>
-  
+   <header>
+    <div class="logo">
+      <img src="/public/author-logo-round.png" alt="Logo Author">
+    </div>
+    <nav class="main-menu">
+      <ul>
+        <li
+          v-for="(item,index) in mainMenu"
+          :key="index"
+        ><a :href="item.href">{{ item.text }}</a></li>
+      </ul>
+    </nav>
+  </header>
 </template>
 
 <script>
+import mainMenu from '../data/mainMenu'
 
+export default {
+  name: 'Header',
+  data(){
+    return{
+      mainMenu
+    }
+  }
+}
 </script>
 
 <style scoped>
+@use 'src/components/scss/_variabiles.scss as *;
 
+header{
+  text-align: center;
+  margin-bottom: 50px;
+  .logo{
+    margin-bottom: 50px;
+  }
+  ul{
+    li{
+      display: inline-block;
+
+      a{
+        color: $link-color;
+        text-transform: capitalize;
+        font-weight: bold;
+        padding: 15px;
+        &:hover{
+          background-color: lighten($primary-color, 65%);
+        }
+      }
+    }
+  }
+}
 </style>
-
---
+++ src/components/data/mainMenu.js
@@ -0,0 +1,30 @@
+export default [
+  {
+    href: '#',
+    text: 'Home'
+  },
+  {
+    href: '#',
+    text: 'About Me'
+  },
+  {
+    href: '#',
+    text: 'Testimonials'
+  },
+  {
+    href: '#',
+    text: 'My Blog'
+  },
+  {
+    href: '#',
+    text: 'Meetups'
+  },
+  {
+    href: '#',
+    text: 'Shop'
+  },
+  {
+    href: '#',
+    text: 'Contact Me'
+  },
+];
