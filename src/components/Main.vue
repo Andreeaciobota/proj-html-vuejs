77b362783f6233772b39b3b7fc821a97bf509d6f
Andreea Ciobota <135983461+Andreeaciobota@users.noreply.github.com>
2023-10-23 16:35:42 +0100

    [Add]:section page

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,5 +1,16 @@
 <template>
-  
+  <div class="page">
+    <div class="section section1">
+      <!-- Contenuto della prima sezione -->
+    </div>
+    <div class="section section2">
+      <!-- Contenuto della seconda sezione -->
+    </div>
+    <div class="section section3">
+      <!-- Contenuto della terza sezione -->
+    </div>
+    <!-- Aggiungi più sezioni secondo necessità -->
+  </div>
 </template>
 
 <script>
@@ -7,6 +18,10 @@
 </script>
 
 <style scoped>
-
-</style>
+.page {
+  display: flex;
+  flex-direction: column;
+  width: 100vw; /* Larghezza della pagina */
+  height: 100vh; /* Altezza della pagina */
+}
 
