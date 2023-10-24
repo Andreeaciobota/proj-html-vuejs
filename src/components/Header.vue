cf4becd03bfdfbb86c64d85ae3bc44711262d483
Andreea Ciobota <135983461+Andreeaciobota@users.noreply.github.com>
2023-10-24 16:49:19 +0100

    [Fix]bugs

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,24 +1,30 @@
 <template>
-   <nav class="navbar navbar-expand-lg navbar-light custom-navbar-bg">
-    <router-link class="navbar-brand" to="/">
-      <img src="/public/author-logo-round-200x205.png" alt="Il Tuo Logo" />
-    </router-link>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
+  <div>
+    <!-- Barra di Navigazione -->
+    <nav class="navbar navbar-expand-lg navbar-light custom-navbar-bg">
+      <router-link class="navbar-brand" to="/">
+        <img src="/public/author-logo-round-200x205.png" alt="Il Tuo Logo" />
+      </router-link>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
 
-    <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
-      <ul class="navbar-nav">
-        <li v-for="item in navItems" :key="item.name" class="nav-item">
-          <router-link class="nav-link" :to="item.link">{{ item.name }}</router-link>
-        </li>
-      </ul>
+      <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+        <ul class="navbar-nav">
+          <li v-for="item in navItems" :key="item.name" class="nav-item">
+            <router-link class="nav-link" :to="item.link">{{ item.name }}</router-link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+
+   
+    <div class="section">
+      <div class="sfondo container">
+        
+      </div>
     </div>
-  </nav>
-  <div class="section">
-
   </div>
-
 </template>
 
 <script>
@@ -42,14 +48,27 @@ export default {
 </script>
 
 <style scoped>
+
 .custom-navbar-bg {
-  background-color:transparent  
+  background-color: transparent;
+  width: 100%; 
+  margin: 0; 
 }
 
 .custom-navbar-bg .nav-link {
-  color: black; 
+  color: black;
 }
-.section{
-  background-image: url('/public/hero-07-2x.jpg');
+
+.section {
+  background-image: url('/public/footer-3.jpg');
+  background-size: cover; 
+  background-position: center; 
+  height: 800px; 
+}
+
+.sfondo {
+  background-size: cover;
+  background-position: center;
+  padding: 20px;
 }
 </style>
